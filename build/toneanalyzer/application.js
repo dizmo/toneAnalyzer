042603736ccc2a46b21661b0ab0ed0961c9a2dcb
Jan von der Assen <jan.vda@gmx.ch>
2016-09-05 18:25:27 +0200

    add remaining pie charts for all tones

--- build/toneanalyzer/application.js
+++ build/toneanalyzer/application.js
@@ -13,28 +13,66 @@ function showBack() {
     dizmo.showBack();
 }
 var emotions = [
-			{ label: "anger", value: 5 },
-			{ label: "disgust", value: 2 },
-			{ label: "fear", value: 6 },
-			{ label: "joy", value: 3 },
-			{ label: "sadness Tern", value: 2 }
+			{ label: "anger", value: 0 },
+			{ label: "disgust", value: 0 },
+			{ label: "fear", value: 0 },
+			{ label: "joy", value: 0 },
+			{ label: "sadness Tern", value: 0 }
 		];
 var emotioncolors = [ "#A90418", "#592684", "#325E2B", "#FFD629", "#086DB2"];
 
-function drawPieChart(div, values, colors) {
+var social = [
+			{ label: "Openness", value: 0 },
+			{ label: "Conscientiousness", value: 0 },
+			{ label: "Extraversion", value: 0 },
+      { label: "Agreeableness", value: 0 },
+      { label: "Emotional Range ", value: 0 }
+		];
+var socialcolors = [ "#A90418", "#592684", "#325E2B", "#FFD629", "#086DB2"];
+
+
+
+
+
+
+var lang = [
+			{ label: "analytical", value: 0 },
+			{ label: "confident", value: 0 },
+			{ label: "tentative", value: 0 }
+		];
+var langcolors = [ "#A90418", "#592684", "#325E2B", "#FFD629", "#086DB2"];
+
+
+function drawPieChart(div, values, colors, label, width) {
   var pie = new d3pie(div, {
   	header: {
   		title: {
-  			text: "Emotions"
+  			text: label,
+        color: "white",
+        font: "VAG Rounded"
   		}
   	},
   	size: {
   		pieOuterRadius: "100%",
-  		canvasHeight: 300
+      // pieInnerRadius: "60%",
+  		canvasHeight: 200,
+      canvasWidth: width
   	},
   	data: {
       content : values,
   	},
+    labels : {
+      mainLabel: {
+        color: "white",
+        font: "VAG Rounded",
+        fontSize: 10
+      },
+      lines: {
+        enabled: true,
+        style: "straight",
+        color: "white" // "segment" or a hex color
+      }
+    },
   	misc: {
   		colors: {
   			segments: colors
@@ -51,26 +89,27 @@ function make_base_auth(user, password) {
 
 function getAnalysisFromText(){
   ajax = $.ajax({
-    type: "GET",
-    url: baseurl + text,
+    type: "POST",
+    url: baseurl,
     dataType: 'json',
+    contentType: 'text/plain',
     async: false,
-    data: '{}',
+    data: text,
     beforeSend: function (xhr){
       xhr.setRequestHeader('Authorization', make_base_auth(username, password));
     },
     success: function (res){
-      console.log(res);
       data = res;
       updateData(res)
-      emotions[0].value = res.document_tone.tone_categories[0].tones[0].score;
-      emotions[1].value = res.document_tone.tone_categories[0].tones[1].score;
-      emotions[2].value = res.document_tone.tone_categories[0].tones[2].score;
-      emotions[3].value = res.document_tone.tone_categories[0].tones[3].score;
-      emotions[4].value = res.document_tone.tone_categories[0].tones[4].score;
-      endlog();
       jQuery('#charts').empty();
-      drawPieChart("charts",emotions, emotioncolors);
+      drawPieChart("charts",emotions, emotioncolors, "emotions", 500);
+      if (lang.show) {
+        drawPieChart("charts",lang, langcolors, "languages", 350);
+
+      }
+      drawPieChart("charts",social, socialcolors, "social", 350);
+
+
     }
   });
 }
@@ -79,9 +118,27 @@ function endlog(){
 }
 function updateData(data) {
   //parse emotions (first category)
-  for (var i = 0; i < data.length; i++) {
-    console.log(data.document_tone.tone_categories[0].tones[i].score);
-
+  var empty = 0;
+  var tones = data.document_tone.tone_categories[0].tones;
+  for (var i = 0; i < tones.length; i++) {
+    emotions[i].value = tones[i].score;
+  }
+  tones = data.document_tone.tone_categories[1].tones;
+  for (var i = 0; i < tones.length; i++) {
+    lang[i].value = tones[i].score;
+    if (tones[i].score == 0) {
+      empty++;
+    }
+    if (empty>1) {
+      lang.show = false;
+    }
+    else {
+      lang.show = true;
+    }
+  }
+  tones = data.document_tone.tone_categories[2].tones;
+  for (var i = 0; i < tones.length; i++) {
+    social[i].value = tones[i].score;
   }
 }
 
@@ -92,11 +149,11 @@ window.document.addEventListener('dizmoready', function() {
         dizmo.showFront();
     };
 
-    dizmo.setHeight(700);
-    dizmo.setWidth(700);
-
+    dizmo.setHeight(811);
+    dizmo.setWidth(935);
+    dizmo.setAttribute('settings/frameopacity', 0);
     DizmoElements('#analyze-button').on('click',function(){
-      text = DizmoElements('#input-text').val();
+      text = jQuery('textarea').val();
       getAnalysisFromText();
     });
 
