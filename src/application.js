6c82f04db8fb775492ce096c585b3705b06287d0
Bhaskar Machcha <bhaskar.machcha@harbingergroup.com>
2016-10-25 20:53:17 +0530

    Incremented version to 0.3
    Updated display name & bundle name to 'IBM Tone Analyzer'
    
    [DD-1731] Fix UI issue for Windows platform

--- src/application.js
+++ src/application.js
@@ -33,12 +33,7 @@ function appendColoredTextFromSelection(){
   desub = sub;
 
   for (var i = 0; i < data.sentences_tone.length; i++) {
-
-
-
-
     jQuery('#text-input').append('<span id="span' + i +'">' +data.sentences_tone[i].text+  '</span><br>');
-
     if (data.sentences_tone[i].tone_categories.length > 0) {
       console.log(data.sentences_tone[i].tone_categories.length);
       if (data.sentences_tone[i].tone_categories[getIndexFromSelection(selectedView)].tones[getIndexFromSelection(sub)].score > 0.75) {
@@ -59,32 +54,32 @@ function appendColoredTextFromSelection(){
 }
 function getBackgroundColor(level){
   var color;
-if (selectedView == "language") {
-  color = 'rgba(153,153,153,';
-}
-else if (selectedView == "social") {
-  color = 'rgba(173,200,55,';
-}
-else {
-  switch (selectedEmotion) {
-    case 'anger':
+  if (selectedView == "language") {
+    color = 'rgba(153,153,153,';
+  }
+  else if (selectedView == "social") {
+    color = 'rgba(173,200,55,';
+  }
+  else {
+    switch (selectedEmotion) {
+      case 'anger':
       color = 'rgba(239,59,69,';
       break;
-    case 'disgust':
+      case 'disgust':
       color = 'rgba(142,73,156,';
       break;
-    case 'fear':
+      case 'fear':
       color = 'rgba(127,137,55,';
       break;
-    case 'joy':
+      case 'joy':
       color = 'rgba(236,209,47,';
       break;
-    case 'sadness':
+      case 'sadness':
       color = 'rgba(88,154,200,';
       break;
 
+    }
   }
-}
 
 
 
@@ -98,40 +93,40 @@ else {
 function getIndexFromSelection(selection){
   switch (selection) {
     case "anger":
-      return 0;
+    return 0;
     case "disgust":
-      return 1;
+    return 1;
     case "fear":
-      return 2;
+    return 2;
     case "joy":
-      return 3;
+    return 3;
     case "sadness":
-      return 4;
+    return 4;
 
     case "analytical":
-      return 0;
+    return 0;
     case "confident":
-      return 1;
+    return 1;
     case "tentative":
-      return 2;
+    return 2;
 
     case "openness":
-      return 0;
+    return 0;
     case "conscientiousness":
-      return 1;
+    return 1;
     case "extraversion":
-      return 2;
+    return 2;
     case "agreeableness":
-      return 3;
+    return 3;
     case "emotionalrange":
-      return 4;
+    return 4;
 
     case "emotion":
-      return 0;
+    return 0;
     case "language":
-      return 1;
+    return 1;
     case "social":
-      return 2;
+    return 2;
   }
 }
 function getAnalysisFromText(){
@@ -214,7 +209,7 @@ function drawProgress(container, p, color, colorlight, label) {
   bar.animate(p);  // Number from 0.0 to 1.0
 
   var id = jQuery(container).attr('id');
-return bar;
+  return bar;
 }
 function debug() {
   globalsettings.emotions = {};
@@ -222,21 +217,21 @@ function debug() {
   globalsettings.social = {};
 
 
-drawProgress(anger, 0.1 , 'rgba(239,59,69,0.4)' , 'rgba(239,59,69,0.3)', 'anger');
-drawProgress(disgust, 0.8 , 'rgba(142,73,156,0.4)' , 'rgba(142,73,156,0.3)', 'disgust');
-drawProgress(fear, 0.32 , 'rgba(127,137,55,0.4)' , 'rgba(127,137,55,0.3)', 'fear');
-drawProgress(joy, 0.1 , 'rgba(236,209,47,0.4)' , 'rgba(236,209,47,0.3)', 'joy');
-drawProgress(sadness, 0.85 , 'rgba(88,154,200,0.4)' , 'rgba(88,154,200,0.3)', 'sadness');
+  drawProgress(anger, 0.1 , 'rgba(239,59,69,0.4)' , 'rgba(239,59,69,0.3)', 'anger');
+  drawProgress(disgust, 0.8 , 'rgba(142,73,156,0.4)' , 'rgba(142,73,156,0.3)', 'disgust');
+  drawProgress(fear, 0.32 , 'rgba(127,137,55,0.4)' , 'rgba(127,137,55,0.3)', 'fear');
+  drawProgress(joy, 0.1 , 'rgba(236,209,47,0.4)' , 'rgba(236,209,47,0.3)', 'joy');
+  drawProgress(sadness, 0.85 , 'rgba(88,154,200,0.4)' , 'rgba(88,154,200,0.3)', 'sadness');
 
-drawProgress(analytical, 0.6 , 'rgba(153,153,153,0.4)' , 'rgba(153,153,153,0.3)','analytical');
-drawProgress(confident, 0.5 , 'rgba(153,153,153,0.4)' , 'rgba(153,153,153,0.3)','confident');
-drawProgress(tentative, 0.2 , 'rgba(153,153,153,0.4)' , 'rgba(153,153,153,0.3)', 'tentative');
+  drawProgress(analytical, 0.6 , 'rgba(153,153,153,0.4)' , 'rgba(153,153,153,0.3)','analytical');
+  drawProgress(confident, 0.5 , 'rgba(153,153,153,0.4)' , 'rgba(153,153,153,0.3)','confident');
+  drawProgress(tentative, 0.2 , 'rgba(153,153,153,0.4)' , 'rgba(153,153,153,0.3)', 'tentative');
 
-drawProgress(openness, 0.7 , 'rgba(173,200,55,0.4)' , 'rgba(173,200,55,0.3)', 'openness');
-drawProgress(conscientiousness, 0.5 , 'rgba(173,200,55,0.4)' , 'rgba(173,200,55,0.3)', 'conscientiousness');
-drawProgress(extraversion, 0.4 , 'rgba(173,200,55,0.4)' , 'rgba(173,200,55,0.3)', 'extraversion');
-drawProgress(agreeableness, 0.7 , 'rgba(173,200,55,0.4)' , 'rgba(173,200,55,0.3)', 'agreeableness');
-drawProgress(emotionalrange, 0.8 , 'rgba(173,200,55,0.4)' , 'rgba(173,200,55,0.3)', 'emotionalrange');
+  drawProgress(openness, 0.7 , 'rgba(173,200,55,0.4)' , 'rgba(173,200,55,0.3)', 'openness');
+  drawProgress(conscientiousness, 0.5 , 'rgba(173,200,55,0.4)' , 'rgba(173,200,55,0.3)', 'conscientiousness');
+  drawProgress(extraversion, 0.4 , 'rgba(173,200,55,0.4)' , 'rgba(173,200,55,0.3)', 'extraversion');
+  drawProgress(agreeableness, 0.7 , 'rgba(173,200,55,0.4)' , 'rgba(173,200,55,0.3)', 'agreeableness');
+  drawProgress(emotionalrange, 0.8 , 'rgba(173,200,55,0.4)' , 'rgba(173,200,55,0.3)', 'emotionalrange');
 
 }
 function setRadioButtonBackgrounds(){
@@ -245,95 +240,97 @@ function setRadioButtonBackgrounds(){
 }
 // As soon as the dom is loaded, and the dizmo is ready, instantiate the main class
 window.document.addEventListener('dizmoready', function() {
-    // Your code should be in here so that it is secured that the dizmo is fully loaded
-    document.getElementById('doneBtn').onclick = function() {
-        dizmo.showFront();
-    };
+  // Your code should be in here so that it is secured that the dizmo is fully loaded
+  document.getElementById('doneBtn').onclick = function() {
+    dizmo.showFront();
+  };
 
-dizmo.setHeight(550);
-dizmo.setWidth(400);
-dizmo.canDock(true);
-dizmo.setAttribute('settings/title', "Tone Analyzer");
-jQuery('#front').css('background-image', 'url(style/background.png)');
-setRadioButtonBackgrounds();
-debug();
-
-
-jQuery('#navigation-tab button').on('click', function(){
-  jQuery('#navigation-tab button').css('background', 'none');
-  jQuery('#navigation-tab button').css('color', 'rgb(87,151,229)');
-
-
-  jQuery(this).css('background-color', 'rgb(87,151,229)');
-  jQuery(this).css('color', 'white');
-  jQuery(this).css('padding-left', '20px');
-
-  jQuery(this).css('padding-right', '20px');
-  jQuery(this).css('border-color', 'rgb(87,151,229)');
-
-  selectedView = jQuery(this).attr('view');
-
-  jQuery('.front-view').hide();
-  jQuery('#' + selectedView).show();
-});
-
-jQuery('input[type=radio]').on('click', function(){
+  dizmo.setHeight(550);
+  dizmo.setWidth(400);
+  dizmo.canDock(true);
+  dizmo.setAttribute('settings/title', "Tone Analyzer");
+  jQuery('#front').css('background-image', 'url(style/background.png)');
   setRadioButtonBackgrounds();
-});
-
-jQuery('input[name=emotion]').on('click', function(){
-  selectedEmotion = jQuery('input[name=emotion]:checked').val();
-  appendColoredTextFromSelection();
-});
-
-jQuery('input[name=language]').on('click', function(){
-  selectedLanguage = jQuery('input[name=language]:checked').val();
-  appendColoredTextFromSelection();
-});
-
-jQuery('input[name=social]').on('click', function(){
-  selectedSocial = jQuery('input[name=social]:checked').val();
-  appendColoredTextFromSelection();
-});
-
-jQuery('#emotion-btn').click();
+  debug();
 
 
-jQuery('#analyse-btn').on('click', function(){
+  jQuery('#navigation-tab button').on('click', function(){
+    jQuery('#navigation-tab button').css('background', 'none');
+    jQuery('#navigation-tab button').css('color', 'rgb(87,151,229)');
 
-  jQuery('#default-display').show();
-  jQuery('#views').hide();
-  jQuery('#navigation-tab').hide();
 
-  text = jQuery('#text-input').text();
-  getAnalysisFromText();
-  jQuery('#default-logo').hide();
-  jQuery('#loading-icon').show();
-  jQuery('svg').remove();
-});
+    jQuery(this).css('background-color', 'rgb(87,151,229)');
+    jQuery(this).css('color', 'white');
+    // jQuery(this).css('padding-left', '20px');
+    jQuery(this).css('padding-left', '10px');
 
-dizmo.onDock(function(dockingDizmo){
-  console.log('Docked to dizmo with id ' + dockingDizmo.identifier);
-  console.log('text: ' + dockingDizmo.publicStorage.getProperty('stdout'));
-  if (dockingDizmo.publicStorage.getProperty('stdout') !== "") {
-    jQuery('#text-input').text(dockingDizmo.publicStorage.getProperty('stdout'));
-    jQuery('#analyse-btn').click();
-  }
+    // jQuery(this).css('padding-right', '20px');
+    jQuery(this).css('padding-right', '7px');
+    jQuery(this).css('border-color', 'rgb(87,151,229)');
 
-  subscriptionId = dockingDizmo.publicStorage.subscribeToProperty('stdout', function(path, val, oldVal) {
-    var stdout = val;
-    // do something with the stdout variable
-    jQuery('#text-input').text(stdout);
-    jQuery('#analyse-btn').click();
+    selectedView = jQuery(this).attr('view');
+
+    jQuery('.front-view').hide();
+    jQuery('#' + selectedView).show();
   });
-});
-dizmo.onUndock(function(undockedDizmo) {
-  dizmo.publicStorage.unsubscribeProperty(subscriptionId);
-});
 
-globalsettings.emotions = {};
-globalsettings.language = {};
-globalsettings.social = {};
+  jQuery('input[type=radio]').on('click', function(){
+    setRadioButtonBackgrounds();
+  });
+
+  jQuery('input[name=emotion]').on('click', function(){
+    selectedEmotion = jQuery('input[name=emotion]:checked').val();
+    appendColoredTextFromSelection();
+  });
+
+  jQuery('input[name=language]').on('click', function(){
+    selectedLanguage = jQuery('input[name=language]:checked').val();
+    appendColoredTextFromSelection();
+  });
+
+  jQuery('input[name=social]').on('click', function(){
+    selectedSocial = jQuery('input[name=social]:checked').val();
+    appendColoredTextFromSelection();
+  });
+
+  jQuery('#emotion-btn').click();
+
+
+  jQuery('#analyse-btn').on('click', function(){
+
+    jQuery('#default-display').show();
+    jQuery('#views').hide();
+    jQuery('#navigation-tab').hide();
+
+    text = jQuery('#text-input').text();
+    getAnalysisFromText();
+    jQuery('#default-logo').hide();
+    jQuery('#loading-icon').show();
+    jQuery('svg').remove();
+  });
+
+  dizmo.onDock(function(dockingDizmo){
+    console.log('Docked to dizmo with id ' + dockingDizmo.identifier);
+    console.log('text: ' + dockingDizmo.publicStorage.getProperty('stdout'));
+    if (dockingDizmo.publicStorage.getProperty('stdout') !== "") {
+      jQuery('#text-input').text(dockingDizmo.publicStorage.getProperty('stdout'));
+      jQuery('#analyse-btn').click();
+    }
+
+    subscriptionId = dockingDizmo.publicStorage.subscribeToProperty('stdout', function(path, val, oldVal) {
+      var stdout = val;
+      // do something with the stdout variable
+      jQuery('#text-input').text(stdout);
+      jQuery('#analyse-btn').click();
+    });
+  });
+  dizmo.onUndock(function(undockedDizmo) {
+    dizmo.publicStorage.unsubscribeProperty(subscriptionId);
+  });
+
+  globalsettings.emotions = {};
+  globalsettings.language = {};
+  globalsettings.social = {};
 
 
 });
